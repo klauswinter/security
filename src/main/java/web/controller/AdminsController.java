b7f112301011eb197387f6bd1021f8a2cefdd0a3
Danil <korotkin321@gmail.com>
2021-11-27 14:42:01 +0300

    Разделил доступ к CRUD операциям, добавил UsersController

---
+++ src/main/java/web/controller/AdminsController.java
@@ -0,0 +1,63 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import web.model.User;
+import web.service.UserServiceImpl;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminsController {
+
+    private final UserServiceImpl userService;
+
+    @Autowired
+    public AdminsController(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping()
+    public String allUsers(Model model) {
+        model.addAttribute("users", userService.getAllUsers());
+        return "admin/index";
+    }
+
+    @GetMapping("/user_info/{id}")
+    public String showUser(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("show_users", userService.getUserById(id));
+        return "admin/user_info";
+    }
+
+    @GetMapping("/new")
+    public String addNewUser(Model model) {
+        model.addAttribute("user", new User());
+        return "admin/new_user";
+    }
+
+    @PostMapping()
+    public String createNewUser(@ModelAttribute("user") User user) {
+        userService.saveUser(user);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editUser(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("user", userService.getUserById(id));
+        return "admin/edit";
+    }
+
+    @PatchMapping("/edit/{id}")
+    public String updateUser(@ModelAttribute("user") User user, @PathVariable("id") Long id) {
+        userService.updateUser(id, user);
+        return "redirect:/admin";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable("id") Long id) {
+        userService.removeUserById(id);
+        return "redirect:/admin";
+    }
+
+}
--- src/main/java/web/controller/UsersController.java
+++ src/main/java/web/controller/UsersController.java
@@ -1,57 +1,25 @@
 package web.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import web.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import web.service.UserServiceImpl;
 
 @Controller
-@RequestMapping("/admin")
+@RequestMapping("/user")
 public class UsersController {
 
     private final UserServiceImpl userService;
 
-    @Autowired
     public UsersController(UserServiceImpl userService) {
         this.userService = userService;
     }
 
-    @GetMapping()
-    public String index(Model model) {
-        model.addAttribute("users", userService.getAllUsers());
-        return "admin/index";
+    @GetMapping("/{id}")
+    public String showUser(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("nonadmin_users", userService.getUserById(id));
+        return "user/user_info";
     }
-
-    @GetMapping("/new")
-    public String addNewPerson(Model model) {
-        model.addAttribute("user", new User());
-        return "admin/new_user";
-    }
-
-    @PostMapping()
-    public String createNewUser(@ModelAttribute("user") User user) {
-        userService.saveUser(user);
-        return "redirect:/admin";
-    }
-
-    @GetMapping("/edit/{id}")
-    public String editUser(@PathVariable("id") long id, Model model) {
-        model.addAttribute("user", userService.getUserById(id));
-        return "admin/edit";
-    }
-
-    @PatchMapping("/edit/{id}")
-    public String updateUser(@ModelAttribute("user") User user, @PathVariable("id") long id) {
-        userService.updateUser(id, user);
-        return "redirect:/admin";
-    }
-
-    @DeleteMapping("/{id}")
-    public String deleteUser(@PathVariable("id") long id) {
-        userService.removeUserById(id);
-        return "redirect:/admin";
-    }
-
 }
